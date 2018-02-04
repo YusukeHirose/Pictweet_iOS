baab52c9876cbd6459ca2a88888af6874165d83e
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-04 14:00:46 +0900

    RegisterCoadWrite

--- Pictweet/Controller/RegisterViewController.swift
+++ Pictweet/Controller/RegisterViewController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let backButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(toBack))
+        navigationItem.leftBarButtonItem = backButtonItem
+        userNameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func toBack() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func didTapRegisterButton(_ sender: UIButton) {
+        guard let userName = userNameTextField.text,
+              let email = emailTextField.text, let password = passwordTextField.text else { return }
+              let user = User(userName: userName, email: email, password: password)
+        
+        user.register { error in
+            if let errorMessage = (error as NSError?)?.userInfo["NSLocalizedDescription"] as? String {
+                self.showAlert(message: errorMessage)
+            } else {
+                print("登録成功")
+            }
+        }
+    }
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
--- Pictweet/Model/User.swift
+++ Pictweet/Model/User.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import NCMB
+
+class User {
+    var userName: String
+    var email: String
+    var password: String
+
+    init(userName: String, email: String, password: String) {
+        self.userName = userName
+        self.email = email
+        self.password = password
+    }
+    
+    func register(callback: @escaping (_ message: Error?) -> Void) {
+        let user = NCMBUser()
+        user.userName = userName
+        user.mailAddress = email
+        user.password = password
+        user.signUpInBackground { error in
+            callback(error)
+        }
+    }
+    
+    func login(callback: @escaping (_ message: Error?) -> Void) {
+        NCMBUser.logInWithUsername(inBackground: self.userName, password: self.password) { user, error in
+            callback(error)
+        }
+    }
+}
